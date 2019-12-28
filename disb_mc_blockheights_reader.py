49e4aa785c9678cffc1567c2b783b6021d0395f3
0xTiger <59308049+0xTiger@users.noreply.github.com>
2019-12-28 15:36:36 +0000

    added readers for visualising

---
+++ disb_mc_blockheights_reader.py
@@ -0,0 +1,66 @@
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import matplotlib.image as mpimg
+from matplotlib.offsetbox import TextArea, DrawingArea, OffsetImage, AnnotationBbox
+
+flatten = lambda l: [item for sublist in l for item in sublist]
+dat = lambda x: [(v[x] if x in v else 0) for _,v in layers.items()]
+
+def dict_merge(dicts):
+    neodict = {}
+    for k, v in reduce(lambda x,y: x + y, [d.items() for d in dicts]):
+        if k in neodict.keys():
+            neodict[k] += v
+        else:
+            neodict[k] = v
+    return neodict
+
+cache_file = 'assets\\mc\\blockheights_backup.json'
+
+print("Loading cache")
+with open(cache_file) as f:
+    cache = json.load(f)
+    layers = {int(k):v for k,v in cache['layers'].items()}
+
+total_counts = dict_merge([v for k,v in layers.items()])
+blocktypes = [{'id': k, 'data': dat(k), 'total': v} for k,v in total_counts.items()]
+blocktypes = sorted(blocktypes, key=lambda x: -1*x['total'])
+
+distribs = [blocktype['data'] for blocktype in blocktypes]
+names  = [blocktype['id'] for blocktype in blocktypes]
+print(names)
+
+print("Plotting Data")
+plt.style.use('ggplot')
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+
+plt.stackplot(range(256), distribs, labels=names)
+
+fig2 = plt.figure()
+ax2 = fig2.add_subplot(1,1,1)
+
+def animate(i):
+    type = blocktypes[i]
+
+    ax2.clear()
+    ax2.plot(range(256), type['data'], label=type['id'], color='#111111')#, color=ore['color'])
+    ax2.fill_between(range(256), type['data'], color='#111111')
+    ax2.legend()
+
+    try:
+        imagebox = OffsetImage(mpimg.imread('assets\\mc\\textures\\' + type['id'] + '.png'), zoom=3)
+        ax2.add_artist(AnnotationBbox(imagebox, (0.870,0.8), xycoords='axes fraction', frameon=False, annotation_clip=True))
+    except:
+        imagebox = OffsetImage(mpimg.imread('assets\\mc\\textures\\missing.png'), zoom=3)
+        ax2.add_artist(AnnotationBbox(imagebox, (0.870,0.8), xycoords='axes fraction', frameon=False, annotation_clip=True))
+
+    plt.xlabel('y level')
+    plt.ylabel('Abundance')
+    plt.title('Distribution of each type \nof block in a minecraft world')
+
+ani = animation.FuncAnimation(fig2, animate, frames=range(len(blocktypes)), interval=500, repeat_delay=2000)
+#ani.save('assets\\mc\\' + "mc_blockheights.mp4")
+plt.show()
---
+++ disb_mc_oreheights_reader.py
@@ -0,0 +1,82 @@
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+flatten = lambda l: [item for sublist in l for item in sublist]
+
+def dict_merge(dicts):
+    neodict = {}
+    for k, v in reduce(lambda x,y: x + y, [d.items() for d in dicts]):
+        if k in neodict.keys():
+            neodict[k] += v
+        else:
+            neodict[k] = v
+    return neodict
+
+cache_file = 'assets\\mc\\blockheights_backup.json'
+
+print("Loading cache")
+with open(cache_file) as f:
+    cache = json.load(f)
+    layers = {int(k):v for k,v in cache['layers'].items()}
+
+
+total_counts = dict_merge([v for k,v in layers.items()])
+allblocktypes = [k for k,v in total_counts.items()]
+print(allblocktypes)
+
+ores = [{'id': 'coal_ore', 'color': '#141212'},
+        {'id': 'iron_ore', 'color': '#f2b28a'},
+        {'id': 'redstone_ore', 'color': '#ff0000'},
+        {'id': 'gold_ore', 'color': '#fce232'},
+        {'id': 'lapis_ore', 'color': '#112fb8'},
+        {'id': 'diamond_ore', 'color': '#33ffeb'},
+        {'id': 'emerald_ore', 'color': '#18d921'}]
+
+pltdata = []
+for ore in ores:
+    id = ore['id']
+    ore['data'] = [(v[id] if id in v else 0) for _,v in layers.items()]
+    pltdata.append(flatten([[k]*v[id] for k,v in layers.items() if id in v]))
+
+maxhs = [max(ore['data']) for ore in ores]
+totals = [sum(ore['data']) for ore in ores]
+print(totals)
+widthss = map(lambda x: float(x) / max(maxhs), maxhs)
+
+print("Plotting Data")
+plt.style.use('ggplot')
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+
+def set_axis_style(ax, labels):
+    ax.get_xaxis().set_tick_params(direction='out')
+    ax.xaxis.set_ticks_position('bottom')
+    ax.set_xticks(np.arange(1, len(labels) + 1))
+    ax.set_xticklabels(labels)
+    ax.set_xlim(0.25, len(labels) + 0.75)
+    ax.set_xlabel('Ore type')
+
+parts = ax.violinplot(
+        pltdata, widths=widthss, showmeans=False, showmedians=False,
+        showextrema=False)
+
+i = 0
+for pc in parts['bodies']:
+    pc.set_facecolor(ores[i]['color'])
+    pc.set_alpha(1)
+    i += 1
+
+set_axis_style(ax, map(lambda x: x.split('_')[0], [ore['id'] for ore in ores]))
+plt.ylabel('y level')
+plt.title('How common are different ores\nin each layer of a minecraft world', fontname='Calibri', fontsize=20)
+
+fig2 = plt.figure()
+ax2 = fig2.add_subplot(1,1,1)
+
+for ore in ores:
+    plt.plot(range(256), ore['data'], label=ore['id'], color=ore['color'])
+plt.legend()
+
+plt.show()
