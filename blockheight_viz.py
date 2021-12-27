0bce6301a7683a04a64426ee9c0f529c60c779bb
0xTiger <tgh.lizard@googlemail.com>
2021-12-27 12:46:10 +0000

    refactor: abstract plot_icon

--- blockheight_viz.py
+++ blockheight_viz.py
@@ -3,17 +3,15 @@ from collections import Counter
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import matplotlib.image as mpimg
-from matplotlib.offsetbox import TextArea, DrawingArea, OffsetImage, AnnotationBbox
+from matplotlib.offsetbox import OffsetImage, AnnotationBbox
 
 dat = lambda x: [v.get(x, 0) for v in layers.values()]
 merge_dicts = lambda dicts: sum((Counter(d) for d in dicts), Counter())
 
 cache_file = 'dataset.json'
 
-print("Loading cache")
 with open(cache_file) as f:
-    cache = json.load(f)
-    layers = cache['layers']
+    layers = json.load(f)['layers']
 
 total_counts = merge_dicts(layers.values())
 blocktypes = [{'id': id, 'data': dat(id), 'total': v} for id, v in total_counts.items()]
@@ -23,30 +21,36 @@ distribs = [blocktype['data'] for blocktype in blocktypes]
 names = [blocktype['id'] for blocktype in blocktypes]
 print(total_counts)
 
-print("Plotting Data")
+
 plt.style.use('ggplot')
 
 fig, ax = plt.subplots()
 plt.stackplot(range(256), distribs, labels=names)
 
+
 fig2, ax2 = plt.subplots()
+def plot_icon(ax, filepath):
+    imagebox = OffsetImage(mpimg.imread(filepath), zoom=5)
+    ax.add_artist(AnnotationBbox(imagebox, (0.985,0.9),
+                                xycoords='axes fraction',
+                                frameon=False,
+                                annotation_clip=True,
+                                box_alignment=(1,1)))
 
 def animate(i):
     type = blocktypes[i]
 
     ax2.clear()
-    ax2.plot(range(256), type['data'], label=type['id'], color='#111111')#, color=ore['color'])
-    ax2.fill_between(range(256), type['data'], color='#111111')
+    ax2.plot(range(256), type['data'], label=type['id'], color='k')#, color=ore['color'])
+    ax2.fill_between(range(256), type['data'], color='k')
     ax2.legend()
 
     try:
-        imagebox = OffsetImage(mpimg.imread('textures/' + type['id'] + '.png'), zoom=5)
-        ax2.add_artist(AnnotationBbox(imagebox, (0.985,0.9), xycoords='axes fraction', frameon=False, annotation_clip=True, box_alignment=(1,1)))
+        plot_icon(ax2, 'textures/' + type['id'] + '.png')
     except FileNotFoundError:
         try:
-            imagebox = OffsetImage(mpimg.imread('textures/missing.png'), zoom=5)
-            ax2.add_artist(AnnotationBbox(imagebox, (0.985,0.9), xycoords='axes fraction', frameon=False, annotation_clip=True, box_alignment=(1,1)))
-        except:
+            plot_icon(ax2, 'textures/missing.png')
+        except FileNotFoundError:
             pass
 
     ax2.set_xlabel('y level')
