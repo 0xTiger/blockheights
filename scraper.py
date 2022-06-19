00c8116fce802eead9513494d98d2cd417b41e04
0xTiger <tgh.lizard@googlemail.com>
2022-06-19 10:13:53 +0100

    refactor: add cli args and minor refactoring

--- scraper.py
+++ scraper.py
@@ -1,13 +1,21 @@
-import os
+import argparse
 import json
-from anvil import Region, Chunk
-from itertools import product
+import os
 from collections import Counter
+from itertools import product
 from timeit import default_timer as timer
+
+from anvil import Chunk, Region
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--world', required=True, help='Name, (not path) of the minecraft world directory')
+parser.add_argument('--out', default='out.json', help='Path to output data file')
+parser.add_argument('--spiral', action='store_true', help='Set flag to scrape regions in a spiral order')
+args = parser.parse_args()
 
 ord = lambda x: x[0]*32 + x[1]
 
-def spiral_iter(t):
+def spiral_step(t):
     x, y = t
     # Starts (0,0) -> (1,0) and continues anti-clockwise
     if x > y > -1*x: y += 1 # Walk UP
@@ -22,22 +30,30 @@ elif os.name == 'posix':
     minecraft_path = os.environ.get("HOME")
 
 
-world = 'myworld'
-cache_file = 'dataset2.json'
-linearPath = True #Determines which regions are chosen for scanning and in which order
+linearPath = not args.spiral # Determines which regions are chosen for scanning and in which order
+world_folder = os.path.join(minecraft_path, '.minecraft/saves', args.world)
 
-
-world_folder = os.path.join(minecraft_path, '.minecraft/saves', world)
 try:
     print("Loading cache")
-    with open(cache_file) as f:
+    with open(args.out) as f:
         cache = json.load(f)
         layers = {int(k):Counter(v) for k,v in cache['layers'].items()}
         latest_c, latest_r = cache['latest']['chunk'], cache['latest']['region']
 except FileNotFoundError:
     print("FileNotFoundError: starting anew")
-    layers = {y: Counter() for y in range(256)}
+    layers = {y: Counter() for y in range(-64, 320)}
     latest_c, latest_r = (0, 0), (0, 0)
+
+
+def iter_regions(start_region=None, spiral_path=False):
+    x, z = (0, 0) if start_region is None else start_region
+    while True:
+        if spiral_path:
+            x, z = spiral_step((x, z))
+        else:
+            z += 1
+        yield x, z
+
 
 print(f'Starting in r: {latest_r}, c: {latest_c}')
 while True: # Steps through regions
@@ -52,7 +68,7 @@ while True: # Steps through regions
             print(f"Failed: {c}, skipping without saving")
             continue
         
-        for coords in product(range(16), range(256), range(16)): # Steps through induvidual blocks
+        for coords in product(range(16), range(-64, 320), range(16)): # Steps through induvidual blocks
             x, y, z = coords
             block = chunk.get_block(*coords)
             layers[y][block.id] += 1
@@ -60,16 +76,17 @@ while True: # Steps through regions
         latest_c = c
         end = timer()
 
-        with open(cache_file, 'w') as o:
+        with open(args.out, 'w') as o:
             json.dump({'latest': {'region': latest_r, 'chunk': latest_c},
                         'layers': layers}, o, indent=2)
 
-        print(f'Scraped: {c} in {end - start:.3f}s, saved cache @ {cache_file}')
+        print(f'Scraped: {c} in {end - start:.3f}s, saved cache @ {args.out}')
 
-    if linearPath:
-        latest_r = (a, b + 1)
-        latest_c = (0, 0)
+    if args.spiral:
+        # Spiral path
+        latest_r = spiral_step(latest_r)
     else:
-        latest_r = spiral_iter(latest_r)
-        latest_c = (0, 0)
+        # Linear Path
+        latest_r = (a, b + 1)
+    latest_c = (0, 0)
     print(f'Moving to region: {latest_r}')
